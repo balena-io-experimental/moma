6e2bbca6bdcd95e626933d3f83f64689d3a79316
Shaun Mulligan <shaun@resin.io>
2016-12-30 19:40:36 +0200

    switched flask to twisted + klein

--- src/main.py
+++ src/main.py
@@ -1,10 +1,17 @@
-from flask import Flask, jsonify, request
+# from flask import Flask, jsonify, request
+from klein import run, route, Klein
+from twisted.internet import defer, task, reactor
+from twisted.web.static import File
+import json
+
 import automationhat
 from settings import defaults
-from flask_cors import CORS, cross_origin
+# from flask_cors import CORS, cross_origin
 
-app = Flask(__name__, static_url_path='', static_folder='ui/build')
-CORS(app)
+# app = Flask(__name__, static_url_path='', static_folder='ui/build')
+# CORS(app)
+
+app = Klein()
 
 val = 0
 
@@ -18,50 +25,51 @@ def number_to_word(id):
     else:
         return None
 
-
-@app.route("/")
-def root():
-    return app.send_static_file('index.html')
+@app.route('/', branch=True)
+def root(request):
+    f = File("./src/ui/build/index.html")
+    f.isLeaf = True
+    return f
 
 @app.route("/api/relay/<id>", methods=['PUT', 'GET'])
-def toggle(id):
+def toggle(request, id):
     id_str = number_to_word(id)
     if id_str:
         if request.method == 'PUT':
             automationhat.relay[id_str].toggle()
-        return jsonify(value=automationhat.relay[id_str].read())
+        return json.dumps({"value":automationhat.relay[id_str].read()})
     else:
-        return jsonify(result={"status": 500})
+        return json.dumps({"status": 500})
 
 @app.route("/api/output/<id>", methods=['PUT', 'GET'])
-def toggleOutput(id):
+def toggleOutput(request, id):
     id_str = number_to_word(id)
     if id_str:
         if request.method == 'PUT':
             automationhat.output[id_str].toggle()
-        return jsonify(value=automationhat.output[id_str].read())
+        return json.dumps({"value": automationhat.output[id_str].read()})
     else:
-        return jsonify(result={"status": 500})
+        return json.dumps({"status": 500})
 
 @app.route("/api/analog/<id>", methods=['GET'])
-def readAnalog(id):
+def readAnalog(request, id):
     id_str = number_to_word(id)
     if id_str:
-        return jsonify(value=automationhat.analog[id_str].read())
+        return json.dumps({"value": automationhat.analog[id_str].read()})
     else:
-        return jsonify(result={"status": 500})
+        return json.dumps({"status": 500})
 
 @app.route("/api/input/<id>", methods=['GET'])
-def readInput(id):
+def readInput(request, id):
     id_str = number_to_word(id)
     if id_str:
-        return jsonify(value=automationhat.input[id_str].read())
+        return json.dumps({"value": automationhat.input[id_str].read()})
     else:
-        return jsonify(result={"status": 500})
+        return json.dumps({"status": 500})
 
 @app.route("/api/settings")
-def settings():
-    return jsonify(defaults())
+def settings(request):
+    return json.dumps(defaults())
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', port=80)
