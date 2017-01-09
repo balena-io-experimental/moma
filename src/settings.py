cebd3ed1f61bd4cfe4cd88675ded75ad3612be99
gaudi <craig@resin.io>
2017-01-09 09:44:17 +0200

    add poll int envar

--- src/settings.py
+++ src/settings.py
@@ -2,7 +2,7 @@ import os
 
 def defaults():
     return({
-    "POLL_INT": 3000,
+    "POLL_INT": os.environ.get('POLL_INT'),
 	"RELAYS": [ os.environ.get('RELAY1'), os.environ.get('RELAY2'), os.environ.get('RELAY3') ],
     "ANALOGS": [ os.environ.get('ANALOG1'), os.environ.get('ANALOG2'), os.environ.get('ANALOG3') ],
     "INPUTS": [ os.environ.get('INPUT1'), os.environ.get('INPUT2') , os.environ.get('INPUT3') ],
